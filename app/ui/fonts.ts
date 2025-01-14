e7c24620a41f089836412ae4935e46fab08f53b2
unknown <michaelangelo4489@gmail.com>
2025-01-14 11:44:29 +0700

    ch6: setting up db

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Plus_Jakarta_Sans, Montserrat, Lusitana } from 'next/font/google';
+
+export const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin']});
+export const montserrat = Montserrat({ subsets: ['latin']});
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin']});
